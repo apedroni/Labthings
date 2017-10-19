d50daf8c810a37b4e6c02a5f9c03107d8eec55ca
apedroni <anpedroni@gmail.com>
2017-10-19 18:25:48 +0200

    First Version

--- Test.py
+++ Test.py
@@ -1,3 +1,4 @@
 import numpy
+
 freqs = numpy.load('/Users/apedroni/Dropbox/EEG_analysis/fooof-master/tutorial/dat/freqs.npy')
 psd = numpy.load('/Users/apedroni/Dropbox/EEG_analysis/fooof-master/tutorial/dat/psd.npy')
