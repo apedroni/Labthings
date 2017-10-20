88298663beb6cdb0de72bdf844f82b90a0a94155
apedroni <anpedroni@gmail.com>
2017-10-20 14:08:46 +0200

    third commit

--- Test.py
+++
@@ -1,5 +0,0 @@
-import numpy
-
-freqs = numpy.load('/Users/apedroni/Dropbox/EEG_analysis/fooof-master/tutorial/dat/freqs.npy')
-psd2 = numpy.load('/Users/apedroni/Dropbox/EEG_analysis/fooof-master/tutorial/dat/psd.npy')
-
---
+++ foof-script.py
@@ -0,0 +1,7 @@
+import fooof as f
+import pacpy as pa
+import neurodsp as nrd
+
+
+
+
